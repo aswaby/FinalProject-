b2eb0e48a64796f45e526a451b1a4e35e1a87531
swaby <swaby_andrew@yahoo.com>
2016-10-10 13:15:39 -0400

    Final Pro

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".clickme").click(function(event){
                $(".target").toggle('slow', function(){
-                  $(".log").text('Back to top');
+                  $(".log").text('Unlocking Potential');
                });
             });
 
--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+	$(".clickme").click(function(event){
+               $(".target").toggle('slow', function(){
+                  $(".log").text('Back to top');
+               });
+            });
 
 });
